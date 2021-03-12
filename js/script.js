34e168c9a6e4ce57eeb39e747e5a90f303c2ef11
ambiesoft <ambiesoft.trueff@gmail.com>
2021-03-12 15:00:26 +0900

    debug

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 // https://www.marorika.com/entry/copy-to-clipboard
+
+function debugAlert(s) {
+  alert(s);
+}
+
 function copyToClipboard() {
     // コピー対象をJavaScript上で変数として定義する
     var copyTarget = document.getElementById("email");
-
+    debugAlert(1);
     // copyTarget.style.visibility = 'visible';
 
     // コピー対象のテキストを選択する
     copyTarget.select();
+    debugAlert(2);
     copyTarget.setSelectionRange(0, 99999); /* For mobile devices */
-
+    debugAlert(3);
     // 選択しているテキストをクリップボードにコピーする
     document.execCommand("copy");
-
+    debugAlert(4);
     // コピーをお知らせする
     alert("コピーしました:" + copyTarget.value);
-
+    debugAlert(5);
     // copyTarget.style.visibility = 'hidden';
 }
 
